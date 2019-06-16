17e5eb5cb8e3bb3a435cc2f7524512b9d2a910dc
Mike Dao <hungdm@gmail.com>
2019-06-16 18:03:48 -0400

    Update PasswordValidator Class

--- src/midtermreviewcodeforpartc/User.java
+++ src/midtermreviewcodeforpartc/User.java
@@ -8,54 +8,69 @@ package midtermreviewcodeforpartc;
 /**
  * A class that models a User for Uno Online.
  * The user has a name and a password.
+ *
  * @author megha, 2019
  */
-public class User 
+public class User implements PasswordValidator
 {
-    private String name;
-    private String password;
-    
-    /**
-     * a constructor that takes in the user's name and password
-     * @param givenName
-     * @param givenPass 
-     */
-    public User(String givenName, String givenPass)
-    {
-        name = givenName;
-        password=givenPass;
-    }
+   private String name;
+   private String password;
 
-    /** The getter for the user name
-     * @return the name
-     */
-    public String getName() {
-        return name;
-    }
+   /**
+    * a constructor that takes in the user's name and password
+    *
+    * @param givenName
+    * @param givenPass
+    */
+   public User (String givenName, String givenPass)
+   {
+      name = givenName;
+      password = givenPass;
+   }
 
-    /**
-     * The setter for the user name
-     * @param givenName the name to set
-     */
-    public void setName(String givenName) {
-        name = givenName;
-    }
+   /** The getter for the user name
+    *
+    * @return the name
+    */
+   public String getName ()
+   {
+      return name;
+   }
 
-    /**
-     * The getter for the password
-     * @return the password
-     */
-    public String getPassword() {
-        return password;
-    }
-    
-    /**
-     * A setter for the password
-     * @param givenPass 
-     */
-    public void setPassword(String givenPass)
-    {
-        password = givenPass;
-    }
-    
+   /**
+    * The setter for the user name
+    *
+    * @param givenName the name to set
+    */
+   public void setName (String givenName)
+   {
+      name = givenName;
+   }
+
+   /**
+    * The getter for the password
+    *
+    * @return the password
+    */
+   public String getPassword ()
+   {
+      return password;
+   }
+
+   /**
+    * A setter for the password
+    *
+    * @param givenPass
+    */
+   public void setPassword (String givenPass)
+   {
+      password = givenPass;
+   }
+
+   @Override
+   public boolean validPassword (String userName, String password)
+   {
+      throw new UnsupportedOperationException("Not supported yet.");
+   }
+
 }
